e8ccd06f19ce031ec2a398609e958993712e8fbc
p1p1ng <you@example.com>
2025-04-30 18:48:19 +0700

    New update

---
+++ belajar-routing/src/Pages/About.js
@@ -0,0 +1,11 @@
+import Aboout_components from '../Components/About_components';
+
+const About = () => {
+    return (
+        <div>
+            <Aboout_components />
+        </div>
+    );
+}
+
+export default About;
