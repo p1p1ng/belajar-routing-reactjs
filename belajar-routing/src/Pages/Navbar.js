d2a62152486bd0844a32f4660058ca849c062df1
p1p1ng <you@example.com>
2025-04-30 19:16:13 +0700

    update

--- belajar-routing/src/Pages/Navbar.js
+++ belajar-routing/src/Pages/Navbar.js
@@ -3,11 +3,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-        </div>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
